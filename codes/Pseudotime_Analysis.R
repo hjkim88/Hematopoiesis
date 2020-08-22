f311c7f44c52544be38c231ccf272b153e496bd8
hjkim88 <firadazer@gmail.com>
2020-08-22 00:57:26 -0500

    More pseudotime analysis codes.

--- codes/Pseudotime_Analysis.R
+++ codes/Pseudotime_Analysis.R
@@ -253,6 +253,357 @@ pseudotime_analysis_trent <- function(Robj_path="C:/Users/hkim8/SJ/HSPC Analyses
     }
   )
   
+  #' @title Pairs plot of Slingshot output
+  #' @name pairs-SlingshotDataSet
+  #'
+  #' @description A tool for quickly visualizing lineages inferred by
+  #'   \code{slingshot}.
+  #'
+  #' @param x a \code{SlingshotDataSet} with results to be plotted.
+  #' @param type character, the type of output to be plotted, can be one of
+  #'   \code{"lineages"}, \code{curves}, or \code{both} (by partial matching), see
+  #'   Details for more.
+  #' @param show.constraints logical, whether or not the user-specified initial
+  #'   and terminal clusters should be specially denoted by green and red dots,
+  #'   respectively.
+  #' @param col character, color vector for points.
+  #' @param pch integer or character specifying the plotting symbol, see
+  #'   \code{\link{par}}.
+  #' @param cex numeric, amount by which points should be magnified, see
+  #'   \code{\link{par}}.
+  #' @param lwd numeric, the line width, see \code{\link{par}}.
+  #' @param ... additional parameters for \code{plot} or \code{axis}, see
+  #'   \code{\link{pairs}}.
+  #' @param labels character, the names of the variables, see \code{\link{pairs}}.
+  #' @param horInd see \code{\link{pairs}}.
+  #' @param verInd see \code{\link{pairs}}.
+  #' @param lower.panel see \code{\link{pairs}}.
+  #' @param upper.panel see \code{\link{pairs}}.
+  #' @param diag.panel see \code{\link{pairs}}.
+  #' @param text.panel see \code{\link{pairs}}.
+  #' @param label.pos see \code{\link{pairs}}.
+  #' @param line.main see \code{\link{pairs}}.
+  #' @param cex.labels see \code{\link{pairs}}.
+  #' @param font.labels see \code{\link{pairs}}.
+  #' @param row1attop see \code{\link{pairs}}.
+  #' @param gap see \code{\link{pairs}}.
+  #'
+  #' @details If \code{type == 'lineages'}, straight line connectors between
+  #'   cluster centers will be plotted. If \code{type == 'curves'}, simultaneous
+  #'   principal curves will be plotted.
+  #'
+  #' @details When \code{type} is not specified, the function will first check the
+  #'   \code{curves} slot and plot the curves, if present. Otherwise,
+  #'   \code{lineages} will be plotted, if present.
+  #'
+  #' @return returns \code{NULL}.
+  #'
+  #' @examples
+  #' data("slingshotExample")
+  #' rd <- slingshotExample$rd
+  #' cl <- slingshotExample$cl
+  #' sds <- slingshot(rd, cl, start.clus = "1")
+  #' pairs(sds, type = 'curves')
+  #'
+  #' @export
+  pairs.SlingshotDataSet <-
+    function (x, type = NULL, show.constraints = FALSE, col = NULL,
+              constraints.col = NULL,
+              pch = 16, cex=1, lwd=2, ...,
+              labels, horInd = seq_len(nc), verInd = seq_len(nc),
+              lower.panel = FALSE, upper.panel = TRUE,
+              diag.panel = NULL, text.panel = textPanel,
+              label.pos = 0.5 + has.diag/3, line.main = 3,
+              cex.labels = NULL, font.labels = 1,
+              row1attop = TRUE, gap = 1) {
+      #####
+      lp.sling <- lower.panel
+      up.sling <- upper.panel
+      panel <- points
+      if(!up.sling){
+        upper.panel <- NULL
+      }else{
+        upper.panel <- panel
+      }
+      if(!lower.panel){
+        lower.panel <- NULL
+      }else{
+        lower.panel <- panel
+      }
+      log = ""
+      sds <- x
+      x <- reducedDim(sds)
+      curves <- FALSE
+      lineages <- FALSE
+      if(is.null(type)){
+        if(length(slingCurves(sds)) > 0){
+          type <- 'curves'
+        }else if(length(slingLineages(sds)) > 0){
+          type <- 'lineages'
+        }else{
+          stop('No lineages or curves detected.')
+        }
+      }else{
+        type <- c('curves','lineages','both')[pmatch(type,
+                                                     c('curves','lineages',
+                                                       'both'))]
+        if(is.na(type)){
+          stop('Unrecognized type argument.')
+        }
+      }
+      if(type %in% c('lineages','both')){
+        lineages <- TRUE
+      }
+      if(type %in% c('curves','both')){
+        curves <- TRUE
+      }
+      if(lineages & (length(slingLineages(sds))==0)){
+        stop('No lineages detected.')
+      }
+      if(curves & (length(slingCurves(sds))==0)){
+        stop('No curves detected.')
+      }
+      if(lineages){
+        forest <- slingAdjacency(sds)
+        clusters <- rownames(forest)
+        nclus <- nrow(forest)
+        centers <- t(vapply(clusters,function(clID){
+          w <- slingClusterLabels(sds)[,clID]
+          return(apply(x, 2, weighted.mean, w = w))
+        }, rep(0,ncol(reducedDim(sds)))))
+        rownames(centers) <- clusters
+        linC <- slingParams(sds)
+      }
+      range.max <- max(apply(x,2,function(xi){
+        r <- range(xi, na.rm = TRUE)
+        return(abs(r[2] - r[1]))
+      }))
+      plot.ranges <- apply(x,2,function(xi){
+        mid <- (max(xi,na.rm = TRUE) + min(xi,na.rm = TRUE))/2
+        return(c(mid - range.max/2, mid + range.max/2))
+      })
+      if(is.null(col)){
+        if(requireNamespace("RColorBrewer", quietly = TRUE)) {
+          cc <- c(RColorBrewer::brewer.pal(9, "Set1")[-c(1,3,6)],
+                  RColorBrewer::brewer.pal(7, "Set2")[-2],
+                  RColorBrewer::brewer.pal(6, "Dark2")[-5],
+                  RColorBrewer::brewer.pal(8, "Set3")[-c(1,2)])
+        } else {
+          cc <- seq_len(100)
+        }
+        col <- cc[apply(slingClusterLabels(sds),1,which.max)]
+      }
+      #####
+      if(doText <- missing(text.panel) || is.function(text.panel))
+        textPanel <-
+        function(x = 0.5, y = 0.5, txt, cex, font)
+          text(x, y, txt, cex = cex, font = font)
+      
+      localAxis <- function(side, x, y, xpd, bg, col=NULL, lwd=NULL, main,
+                            oma, ...) {
+        ## Explicitly ignore any color argument passed in as
+        ## it was most likely meant for the data points and
+        ## not for the axis.
+        xpd <- NA
+        if(side %% 2L == 1L && xl[j]) xpd <- FALSE
+        if(side %% 2L == 0L && yl[i]) xpd <- FALSE
+        if(side %% 2L == 1L) Axis(x, side = side, xpd = xpd, ...)
+        else Axis(y, side = side, xpd = xpd, ...)
+      }
+      
+      localPlot <- function(..., main, oma, font.main, cex.main) plot(...)
+      localLowerPanel <- function(..., main, oma, font.main, cex.main)
+        lower.panel(...)
+      localUpperPanel <- function(..., main, oma, font.main, cex.main)
+        upper.panel(...)
+      localDiagPanel <- function(..., main, oma, font.main, cex.main)
+        diag.panel(...)
+      
+      dots <- list(...); nmdots <- names(dots)
+      if (!is.matrix(x)) {
+        x <- as.data.frame(x)
+        for(i in seq_along(names(x))) {
+          if(is.factor(x[[i]]) || is.logical(x[[i]]))
+            x[[i]] <- as.numeric(x[[i]])
+          if(!is.numeric(unclass(x[[i]])))
+            stop("non-numeric argument to 'pairs'")
+        }
+      } else if (!is.numeric(x)) stop("non-numeric argument to 'pairs'")
+      panel <- match.fun(panel)
+      if((has.lower <- !is.null(lower.panel)) && !missing(lower.panel))
+        lower.panel <- match.fun(lower.panel)
+      if((has.upper <- !is.null(upper.panel)) && !missing(upper.panel))
+        upper.panel <- match.fun(upper.panel)
+      if((has.diag  <- !is.null( diag.panel)) && !missing( diag.panel))
+        diag.panel <- match.fun( diag.panel)
+      
+      if(row1attop) {
+        tmp <- lower.panel; lower.panel <- upper.panel; upper.panel <- tmp
+        tmp <- has.lower; has.lower <- has.upper; has.upper <- tmp
+      }
+      
+      nc <- ncol(x)
+      if (nc < 2L) stop("only one column in the argument to 'pairs'")
+      if(!all(horInd >= 1L & horInd <= nc))
+        stop("invalid argument 'horInd'")
+      if(!all(verInd >= 1L & verInd <= nc))
+        stop("invalid argument 'verInd'")
+      if(doText) {
+        if (missing(labels)) {
+          labels <- colnames(x)
+          if (is.null(labels)) labels <- paste("var", 1L:nc)
+        }
+        else if(is.null(labels)) doText <- FALSE
+      }
+      oma <- if("oma" %in% nmdots) dots$oma
+      main <- if("main" %in% nmdots) dots$main
+      if (is.null(oma))
+        oma <- c(4, 4, if(!is.null(main)) 6 else 4, 4)
+      opar <- par(mfrow = c(length(horInd), length(verInd)),
+                  mar = rep.int(gap/2, 4), oma = oma)
+      on.exit(par(opar))
+      dev.hold(); on.exit(dev.flush(), add = TRUE)
+      
+      xl <- yl <- logical(nc)
+      if (is.numeric(log)) xl[log] <- yl[log] <- TRUE
+      else {xl[] <- grepl("x", log); yl[] <- grepl("y", log)}
+      for (i in if(row1attop) verInd else rev(verInd))
+        for (j in horInd) {
+          l <- paste0(ifelse(xl[j], "x", ""), ifelse(yl[i], "y", ""))
+          localPlot(x[, j], x[, i], xlab = "", ylab = "",
+                    axes = FALSE, type = "n", ..., log = l,
+                    xlim = plot.ranges[,j], ylim = plot.ranges[,i])
+          if(i == j || (i < j && has.lower) || (i > j && has.upper) ) {
+            box()
+            if(i == 1  && (!(j %% 2L) || !has.upper || !has.lower ))
+              localAxis(1L + 2L*row1attop, x[, j], x[, i], ...)
+            if(i == nc && (  j %% 2L  || !has.upper || !has.lower ))
+              localAxis(3L - 2L*row1attop, x[, j], x[, i], ...)
+            if(j == 1  && (!(i %% 2L) || !has.upper || !has.lower ))
+              localAxis(2L, x[, j], x[, i], ...)
+            if(j == nc && (  i %% 2L  || !has.upper || !has.lower ))
+              localAxis(4L, x[, j], x[, i], ...)
+            mfg <- par("mfg")
+            if(i == j) {
+              if (has.diag) localDiagPanel(as.vector(x[, i]), ...)
+              if (doText) {
+                par(usr = c(0, 1, 0, 1))
+                if(is.null(cex.labels)) {
+                  l.wid <- strwidth(labels, "user")
+                  cex.labels <- max(0.8, min(2, .9 / max(l.wid)))
+                }
+                xlp <- if(xl[i]) 10^0.5 else 0.5
+                ylp <- if(yl[j]) 10^label.pos else label.pos
+                text.panel(xlp, ylp, labels[i],
+                           cex = cex.labels, font = font.labels)
+              }
+            } else if(i < j){
+              if(up.sling){
+                points(as.vector(x[, j]), as.vector(x[, i]),
+                       col = col, cex = cex, pch=pch, ...)
+                if(lineages){
+                  for(ii in seq_len(nclus-1)){
+                    for(jj in seq(ii+1,nclus)){
+                      if(forest[ii,jj]==1){
+                        seg.col <- 1
+                        lines(centers[c(ii,jj),j],
+                              centers[c(ii,jj),i],
+                              lwd = lwd, col = seg.col, ...)
+                      }
+                    }
+                  }
+                  points(centers[,j],centers[,i], pch = pch,
+                         cex=2*cex)
+                  if(show.constraints && is.null(constraints.col)){
+                    if(any(linC$start.given)){
+                      st.ind <- clusters %in%
+                        linC$start.clus[linC$start.given]
+                      points(centers[st.ind,j],
+                             centers[st.ind,i], cex = cex,
+                             col = 'green3',
+                             pch = pch)
+                    }
+                    if(any(linC$end.given)){
+                      en.ind <- clusters %in%
+                        linC$end.clus[linC$end.given]
+                      points(centers[en.ind,j],
+                             centers[en.ind,i], cex = cex,
+                             col = 'red2', pch = pch)
+                    }
+                  } else if(show.constraints && !is.null(constraints.col)){
+                    for(const in names(constraints.col)) {
+                      points(centers[clusters %in% const, j, drop=FALSE],
+                             centers[clusters %in% const, i, drop=FALSE],
+                             cex = cex, pch = 16,
+                             col = constraints.col[const])
+                    }
+                  }
+                }
+                if(curves){
+                  for(c in slingCurves(sds)){
+                    lines(c$s[c$ord,c(j,i)], lwd = lwd,
+                          col=1, ...)
+                  }
+                }
+              }
+            }
+            else{
+              if(lp.sling){
+                points(as.vector(x[, j]), as.vector(x[, i]),
+                       col = col, cex = cex, pch=pch, ...)
+                if(lineages){
+                  for(ii in seq_len(nclus-1)){
+                    for(jj in seq(ii+1,nclus)){
+                      if(forest[ii,jj]==1){
+                        if(clusters[ii] %in%
+                           linC$start.clus |
+                           clusters[jj] %in%
+                           linC$start.clus){
+                          seg.col <- 'green3'
+                        }else if(clusters[ii] %in%
+                                 linC$end.clus[
+                                   linC$end.given] |
+                                 clusters[jj] %in%
+                                 linC$end.clus[
+                                   linC$end.given]){
+                          seg.col <- 'red2'
+                        }else{
+                          seg.col <- 1
+                        }
+                        lines(centers[c(ii,jj),j],
+                              centers[c(ii,jj),i],
+                              lwd = lwd, col = seg.col,...)
+                      }
+                    }
+                  }
+                  points(centers[,j],centers[,i], pch = pch,
+                         cex = 2*cex)
+                }
+                if(curves){
+                  for(c in slingCurves(sds)){
+                    lines(c$s[c$ord,c(j,i)],lwd = lwd,
+                          col=1, ...)
+                  }
+                }
+              }
+            }
+            if (any(par("mfg") != mfg))
+              stop("the 'panel' function made a new plot")
+          } else par(new = FALSE)
+          
+        }
+      if (!is.null(main)) {
+        font.main <- if("font.main" %in% nmdots){
+          dots$font.main
+        }else par("font.main")
+        cex.main <- if("cex.main" %in%
+                       nmdots) dots$cex.main else par("cex.main")
+        mtext(main, 3, line.main, outer=TRUE, at = 0.5, cex = cex.main,
+              font = font.main)
+      }
+      invisible(NULL)
+    }
+  
   #' @name plot3d-SlingshotDataSet
   #' @title Plot Slingshot output in 3D
   #'
@@ -470,7 +821,7 @@ pseudotime_analysis_trent <- function(Robj_path="C:/Users/hkim8/SJ/HSPC Analyses
     par3d(windowRect = c(50, 50, width+50, height+50))
     plot3d.SlingshotDataSet(slingshot_obj, dims = 1:3, col = "black", col2 = color, type = "lineages", add = TRUE)
     plot3d(slingshot_obj@reducedDim, col = apply(slingshot_obj@clusterLabels, 1, function(x) color[names(x)[which(x == 1)]]),
-           size = 8, aspect = FALSE, add = TRUE)
+           size = 5, alpha = 0.5, aspect = FALSE, add = TRUE)
     axes3d(edges=c("x+-", "y+-", "z++"), lwd = 2,
            labels=TRUE, tick = FALSE, nticks = 3, box = TRUE, expand = 1.05)
     mtext3d(text = expression(bold("PC1")), edge="x+-", line = -2, at = min(slingshot_obj@reducedDim[,1]), pos = NA)
@@ -478,12 +829,12 @@ pseudotime_analysis_trent <- function(Robj_path="C:/Users/hkim8/SJ/HSPC Analyses
     mtext3d(text = expression(bold("PC3")), edge="z++", line = -2, at = max(slingshot_obj@reducedDim[,3]), pos = NA)
     decorate3d(xlim = NULL, ylim = NULL, zlim = NULL, 
                xlab = "", ylab = "", zlab = "", 
-               box = FALSE, axes = FALSE, main = expression(bold(title)), sub = NULL,
+               box = FALSE, axes = FALSE, main = title, sub = NULL,
                top = TRUE, aspect = FALSE, expand = 1.05)
     legend3d("topright", legend = names(color), title = "Clusters",
              col = color, pch = 19, cex=2)
     if(print) {
-      writeWebGL(dir=outputDir, filename = paste0(title, ".html"),
+      writeWebGL(dir=outputDir, filename = paste0(outputDir, title, ".html"),
                  width=width, height = height)
     }
     
@@ -546,19 +897,28 @@ pseudotime_analysis_trent <- function(Robj_path="C:/Users/hkim8/SJ/HSPC Analyses
              pch = 19)
       dev.off()
       
+      ### Trajectory inference on multi dimentional PCA
+      png(paste0(outputDir2, "Trajectory_Inference_", tissue_name, "_Multi-PCA.png"), width = 2500, height = 1500, res = 200)
+      pairs(slingshot_obj, type="lineages", col = apply(slingshot_obj@clusterLabels, 1, function(x) cell_colors_clust[names(x)[which(x == 1)]]),
+            show.constraints = TRUE, constraints.col = cell_colors_clust, cex = 0.8,
+            horInd = 1:5, verInd = 1:5, main="Trajectory Inference Based On Seurat Clusters (Multiple PCs)")
+      par(xpd = TRUE)
+      legend("bottomleft", legend = names(cell_colors_clust), col = cell_colors_clust,
+             pch = 19, title = "Time")
+      dev.off()
+      
       ### 3D Slingshot
       slingshot_3d_lineages(slingshot_obj = slingshot_obj,
                             color = cell_colors_clust,
-                            title = paste0("Trajectory_Inference_3D_PCA_", tissue_name),
+                            title = "Trajectory Inference Based On Seurat Clusters (3D PCA)",
                             print = TRUE,
                             outputDir = outputDir2,
                             width = 1200,
                             height = 800)
+      rgl.close()
       
-      ###
-      plotGenePseudotime(slingshot_obj, "Xkr4", as.matrix(Seurat_Obj@assays$RNA@counts))
-      
-      ### multi dimensional plot - pseudotime
+      ### See specific gene expressions over lineages
+      # plotGenePseudotime(slingshot_obj, "Gene_Name", as.matrix(Seurat_Obj@assays$RNA@counts))
       
     }
     
