c6329db6e7acb9fdac3c5477c042226e935c1872
hjkim88 <firadazer@gmail.com>
2020-08-31 03:32:14 -0500

    RNAMagnet scatter plots completed.

--- codes/RNAMagnet_Analysis.R
+++ codes/RNAMagnet_Analysis.R
@@ -31,11 +31,13 @@
 #
 #   Example
 #               > source("The_directory_of_RNAMagnet_Analysis.R/RNAMagnet_Analysis.R")
-#               > rna_magnet_trent(Seurat_RObj_path="./data/Combined_BM_Seurat_Obj.RDATA",
+#               > rna_magnet_trent(Robj_path="C:/Users/hkim8/SJ/HSPC Analyses/Stroma and Heme/",
+#                                  Seurat_RObj_path="./data/Combined_BM_Seurat_Obj.RDATA",
 #                                  outputDir="./results/RNA-Magnet/")
 ###
 
-rna_magnet_trent <- function(Seurat_RObj_path="./data/Combined_BM_Seurat_Obj.RDATA",
+rna_magnet_trent <- function(Robj_path="C:/Users/hkim8/SJ/HSPC Analyses/Stroma and Heme/",
+                             Seurat_RObj_path="./data/Combined_BM_Seurat_Obj.RDATA",
                              outputDir="./results/RNA-Magnet/") {
   
   ### load libraries
@@ -68,6 +70,14 @@ rna_magnet_trent <- function(Seurat_RObj_path="./data/Combined_BM_Seurat_Obj.RDA
     install.packages("ggplot2")
     require(ggplot2, quietly = TRUE)
   }
+  if(!require(scales, quietly = TRUE)) {
+    install.packages("scales")
+    library(scales, quietly = TRUE)
+  }
+  if(!require(gridExtra, quietly = TRUE)) {
+    install.packages("gridExtra")
+    require(gridExtra, quietly = TRUE)
+  }
   
   ### see python environment since RNAMagnet uses the python module 'magic'
   # conda_create("r-reticulate")
@@ -76,78 +86,99 @@ rna_magnet_trent <- function(Seurat_RObj_path="./data/Combined_BM_Seurat_Obj.RDA
   py_config()
   py_module_available("magic")
   
-  ### load the Seurat object and save the object name
-  tmp_env <- new.env()
-  load(Seurat_RObj_path, tmp_env)
-  obj_name <- ls(tmp_env)
-  assign("Seurat_Obj", get(obj_name, envir = tmp_env))
-  rm(tmp_env)
-  gc()
+  ### get Robj file list
+  f_list <- list.files(Robj_path, pattern = ".Robj$")
   
   ### determine necessary variables
   time_points <- c("E16", "E18", "P0", "ADULT")
   cell_type <- c("Heme", "Stroma")
+  possible_names <- as.vector(sapply(cell_type, function(x) paste0(x, time_points)))
+  possible_names_mat <- data.frame(Names=possible_names,
+                                   Type=as.vector(sapply(cell_type, function(x) rep(x, length(time_points)))),
+                                   Time=rep(time_points, length(cell_type)),
+                                   stringsAsFactors = FALSE, check.names = FALSE)
+  row.names(possible_names_mat) <- possible_names
   
-  ### active assay = "RNA"
-  Seurat_Obj@active.assay <- "RNA"
+  ### keep the required files only
+  f_list <- f_list[which(f_list %in% paste0(possible_names, "_regress.Robj"))]
   
-  ### preprocessing for PCA and UMAP
-  Seurat_Obj <- FindVariableFeatures(Seurat_Obj)
-  Seurat_Obj <- ScaleData(Seurat_Obj)
+  ### a function for color brewer
+  cell_pal <- function(cell_vars, pal_fun) {
+    if (is.numeric(cell_vars)) {
+      pal <- pal_fun(100)
+      return(pal[cut(cell_vars, breaks = 100)])
+    } else {
+      categories <- sort(unique(cell_vars))
+      pal <- setNames(pal_fun(length(categories)), categories)
+      return(pal[cell_vars])
+    }
+  }
   
-  ### run PCA
-  Seurat_Obj <- RunPCA(Seurat_Obj, npcs = 15)
-  
-  ### run UMAP
-  Seurat_Obj <- RunUMAP(Seurat_Obj, dims = 1:5)
-  
-  ### order the data by time
-  Seurat_Obj@meta.data <- Seurat_Obj@meta.data[order(factor(Seurat_Obj@meta.data$Development,
-                                                            levels = time_points)),]
-  
-  ### rownames in the meta.data should be in the same order as colnames in the counts
-  Seurat_Obj@meta.data <- Seurat_Obj@meta.data[colnames(Seurat_Obj@assays$RNA@counts),]
-  
-  ### set the ident of the object with the HSPC type
-  Seurat_Obj <- SetIdent(object = Seurat_Obj,
-                         cells = rownames(Seurat_Obj@meta.data),
-                         value = Seurat_Obj@meta.data$Tissue)
-  
-  ### check whether the orders are the same
-  print(identical(names(Idents(object = Seurat_Obj)), rownames(Seurat_Obj@meta.data)))
-  
-  ### for each R object, perform RNAMagnet
-  for(obj in unique(Seurat_Obj@meta.data$Tissue)) {
+  ### for each of the Robj file, run the RNAMagnet
+  Combined_Seurat_Obj <- NULL
+  for(f in f_list) {
     
-    ### new output directory
-    outputDir2 <- paste0(outputDir, obj, "/")
-    dir.create(outputDir2, showWarnings = FALSE, recursive = TRUE)
-    
-    ### split the Seurat obj based on obj info
-    subset_Seurat_Obj <- subset(Seurat_Obj, idents=obj)
-    
-    ### check whether the orders are the same
-    print(identical(names(Idents(object = subset_Seurat_Obj)), rownames(subset_Seurat_Obj@meta.data)))
+    ### load the Seurat object and save the object name
+    tmp_env <- new.env()
+    load(paste0(Robj_path, f), tmp_env)
+    obj_name <- ls(tmp_env)
+    assign("Seurat_Obj", get(obj_name, envir = tmp_env))
+    rm(tmp_env)
+    gc()
     
     ### rownames in the meta.data should be in the same order as colnames in the counts
-    subset_Seurat_Obj@meta.data <- subset_Seurat_Obj@meta.data[colnames(subset_Seurat_Obj@assays$RNA@counts),]
+    Seurat_Obj@meta.data <- Seurat_Obj@meta.data[colnames(Seurat_Obj@assays$RNA@counts),]
     
-    ### clustering on the R object
-    subset_Seurat_Obj <- FindNeighbors(subset_Seurat_Obj, dims = 1:5, k.param = ifelse(nrow(subset_Seurat_Obj@meta.data) > 5, 5, nrow(subset_Seurat_Obj@meta.data)))
-    subset_Seurat_Obj <- FindClusters(subset_Seurat_Obj, resolution = 0.4)
-    subset_Seurat_Obj@meta.data$new_clusts <- Idents(subset_Seurat_Obj)
+    ### active assay = "RNA"
+    Seurat_Obj@active.assay <- "RNA"
     
-    ### There should be at least two clusters for RNAMagnet
-    if(length(unique(subset_Seurat_Obj@meta.data$new_clusts)) > 1) {
+    ### set output directory
+    outputDir2 <- paste0(outputDir, f, "/")
+    dir.create(outputDir2, recursive = TRUE, showWarnings = FALSE)
+    
+    ### annotate the file name to the R object
+    tissue_name <- strsplit(f, split = "_", fixed = TRUE)[[1]][1]
+    Seurat_Obj@meta.data$Tissue <- tissue_name
+    
+    ### annotate the development
+    Seurat_Obj@meta.data$Development <- possible_names_mat[tissue_name, "Time"]
+    
+    ### annotate the Cell type
+    Seurat_Obj@meta.data$Cell_Type <- possible_names_mat[tissue_name, "Type"]
+    
+    ### combine the Seurat R objects
+    if(is.null(Combined_Seurat_Obj)) {
+      Combined_Seurat_Obj <- Seurat_Obj
+    } else {
+      Combined_Seurat_Obj <- merge(x = Combined_Seurat_Obj, y = Seurat_Obj,
+                                   project = Seurat_Obj@project.name)
+    }
+    
+    ### run only if there are enough number of cells in the R object
+    if(nrow(Seurat_Obj@meta.data) > 1 && identical(names(Idents(object = Seurat_Obj)), rownames(Seurat_Obj@meta.data))) {
+      
+      ### clustering on the R object
+      Seurat_Obj <- FindNeighbors(Seurat_Obj, dims = 1:5, k.param = 5)
+      Seurat_Obj <- FindClusters(Seurat_Obj, resolution = 0.4)
+      Seurat_Obj@meta.data$new_clusts <- Idents(Seurat_Obj)
+      
+      #
+      ### run RNAMagnet
+      #
+      ### get PCA matrix
+      pca_map <- Embeddings(Seurat_Obj, reduction = "pca")[rownames(Seurat_Obj@meta.data),1:5]
       
       ### set the ident of the seurat object with the cluster info
-      subset_Seurat_Obj <- SetIdent(object = subset_Seurat_Obj,
-                                    cells = rownames(subset_Seurat_Obj@meta.data),
-                                    value = subset_Seurat_Obj@meta.data$new_clusts)
+      Seurat_Obj <- SetIdent(object = Seurat_Obj,
+                             cells = rownames(Seurat_Obj@meta.data),
+                             value = Seurat_Obj@meta.data$new_clusts)
       
       ### run RNAMagnet
-      result <- RNAMagnetAnchors(subset_Seurat_Obj,
-                                 anchors = unique(subset_Seurat_Obj@meta.data$new_clusts))
+      result <- RNAMagnetAnchors(Seurat_Obj,
+                                 anchors = levels(Seurat_Obj@meta.data$new_clusts))
+      
+      ### make file name
+      obj <- strsplit(f, split = "_", fixed = TRUE)[[1]][1]
       
       ### write the result as an Excel file
       write.xlsx2(data.frame(Cell=rownames(result), result,
@@ -156,54 +187,88 @@ rna_magnet_trent <- function(Seurat_RObj_path="./data/Combined_BM_Seurat_Obj.RDA
                   sheetName = paste0("RNAMagnet_", obj),
                   row.names = FALSE)
       
-      ### RNAMagnet on PCA
-      qplot(x =Embeddings(subset_Seurat_Obj,reduction="pca")[,1],
-            y=Embeddings(subset_Seurat_Obj,reduction="pca")[,2],
-            color = direction,
-            size=I(0.75),
-            alpha= adhesiveness,data=result) +
-        scale_color_brewer(name = "RNAMagnet\nLocation",palette= "Set1") +
-        scale_alpha_continuous(name = "RNAMagnet\nAdhesiveness") +
-        theme_bw() +
-        theme(panel.grid = element_blank(),
-              axis.text = element_blank(),
-              axis.title = element_blank())
-      
       ### add RNAMagnet info to the seurat object
-      subset_Seurat_Obj@meta.data$direction <- result[rownames(subset_Seurat_Obj@meta.data),"direction"]
-      subset_Seurat_Obj@meta.data$adhesiveness <- result[rownames(subset_Seurat_Obj@meta.data),"adhesiveness"]
+      Seurat_Obj@meta.data$direction <- as.character(result[rownames(Seurat_Obj@meta.data),"direction"])
+      Seurat_Obj@meta.data$adhesiveness <- as.numeric(result[rownames(Seurat_Obj@meta.data),"adhesiveness"])
+      Seurat_Obj@meta.data$specificity <- as.numeric(sapply(rownames(Seurat_Obj@meta.data), function(x) {
+        result[x, paste0("X", result[x,"direction"])]
+      }))
       
-      ### existing clusters
-      cluster_list <- intersect(levels(subset_Seurat_Obj@meta.data$new_clusts),
-                                as.character(unique(subset_Seurat_Obj@meta.data$new_clusts)))
+      ### check the order
+      print(identical(rownames(Seurat_Obj@meta.data), rownames(pca_map)))
       
-      ### for each cluster generate plots
-      for(clust in cluster_list) {
-        
-        ### make a data frame for ggplot
-        plot_df <- data.frame(X=subset_Seurat_Obj@reductions$pca@cell.embeddings[,"PC_1"],
-                              Y=subset_Seurat_Obj@reductions$pca@cell.embeddings[,"PC_2"],
-                              group_color = subset_Seurat_Obj@meta.data$direction,
-                              group_shape = result[rownames(subset_Seurat_Obj@meta.data),paste0("X", clust)],
-                              group_alpha = subset_Seurat_Obj@meta.data$adhesiveness)
-        
-        ### draw a scatter plot with the info
-        ggplot(plot_df, aes_string(x="X", y="Y")) +
-          geom_point(aes_string(col="group_shape", shape="group_color", alpha="group_alpha"), size=2, alpha=0.6) +
-          xlab("") + ylab("") +
-          ggtitle("") +
-          theme_classic(base_size = 16) +
-          theme(legend.position = "bottomleft", legend.title = element_blank(),
-                plot.title = element_text(hjust = 0.5),
-                legend.spacing.y = unit(0, "mm"),
-                legend.background = element_blank(),
-                legend.box.background = element_rect(colour = "gray50"))
-        
-        
-        
-      }
-        
-    } 
+      ### make a data frame for ggplot
+      plot_df <- data.frame(X=pca_map[rownames(Seurat_Obj@meta.data),"PC_1"],
+                            Y=pca_map[rownames(Seurat_Obj@meta.data),"PC_2"],
+                            group_color = Seurat_Obj@meta.data$direction,
+                            group_alpha = Seurat_Obj@meta.data$adhesiveness,
+                            cluster_color = Seurat_Obj@meta.data$new_clusts,
+                            specificity = Seurat_Obj@meta.data$specificity,
+                            stringsAsFactors = FALSE, check.names = FALSE)
+      
+      ### factorize the direction columns
+      plot_df$group_color <- factor(plot_df$group_color, levels = levels(plot_df$cluster_color)) 
+      
+      ### get colors for the clustering result
+      cell_colors_clust <- cell_pal(levels(Seurat_Obj@meta.data$new_clusts), hue_pal())
+      
+      ### scatter plot
+      p <- list()
+      
+      ### original PCA
+      p[[1]] <- ggplot(plot_df, aes_string(x="X", y="Y")) +
+        geom_point(aes_string(col="cluster_color"), size=2) +
+        xlab("PC1") + ylab("PC2") +
+        labs(col="Cluster") +
+        ggtitle("PCA with Cluster Info") +
+        theme_classic(base_size = 16) +
+        scale_color_manual(values = cell_colors_clust, labels = names(cell_colors_clust))
+      p[[1]] <- LabelClusters(plot = p[[1]], id = "cluster_color")
+      
+      ### draw a scatter plot with the adhesiveness info
+      p[[2]] <- ggplot(plot_df, aes_string(x="X", y="Y")) +
+        geom_point(aes_string(col="group_color", alpha="group_alpha"), size=2) +
+        xlab("PC1") + ylab("PC2") +
+        labs(col="Direction", alpha="Adhesiveness") +
+        ggtitle("PCA with Direction & Adhesiveness") +
+        theme_classic(base_size = 16) +
+        scale_color_manual(values = cell_colors_clust[as.character(unique(plot_df$group_color)[order(unique(plot_df$group_color))])],
+                           labels = names(cell_colors_clust[as.character(unique(plot_df$group_color)[order(unique(plot_df$group_color))])]))
+      p[[2]] <- LabelClusters(plot = p[[2]], id = "cluster_color")
+      
+      ### save the plots
+      g <- arrangeGrob(grobs = p,
+                       nrow = 2,
+                       ncol = 1,
+                       top = "")
+      ggsave(file = paste0(outputDir2, "PCA_RNAMagnet_Result_AD_", obj, ".png"), g, width = 20, height = 12, dpi = 300)
+      
+      ### draw a scatter plot with the specificity info
+      p[[2]] <- ggplot(plot_df, aes_string(x="X", y="Y")) +
+        geom_point(aes_string(col="group_color", alpha="specificity"), size=2) +
+        xlab("PC1") + ylab("PC2") +
+        labs(col="Direction", alpha="Specificity Score") +
+        ggtitle("PCA with Direction & Specificity Score") +
+        theme_classic(base_size = 16) +
+        scale_color_manual(values = cell_colors_clust[as.character(unique(plot_df$group_color)[order(unique(plot_df$group_color))])],
+                           labels = names(cell_colors_clust[as.character(unique(plot_df$group_color)[order(unique(plot_df$group_color))])]))
+      p[[2]] <- LabelClusters(plot = p[[2]], id = "cluster_color")
+      
+      ### save the plots
+      g <- arrangeGrob(grobs = p,
+                       nrow = 2,
+                       ncol = 1,
+                       top = "")
+      ggsave(file = paste0(outputDir2, "PCA_RNAMagnet_Result_SP_", obj, ".png"), g, width = 20, height = 12, dpi = 300)
+      
+      #
+      ### heatmap with specificity scores - col side color bars with direction & adhesiveness
+      #
+      
+      
+      
+      
+    }
     
   }
   
