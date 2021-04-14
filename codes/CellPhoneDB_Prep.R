23a32c6cd84d4987e1a16c4d086db392cdc7fcf6
hjkim88 <firadazer@gmail.com>
2021-04-14 18:33:21 -0500

    mouse gene to human gene conversion.

--- codes/CellPhoneDB_Prep.R
+++ codes/CellPhoneDB_Prep.R
@@ -29,6 +29,50 @@ cpdb_prep <- function(Robj_dir="Z:/ResearchHome/ProjectSpace/thomagrp/JCC282_Hem
     get(ls()[ls() != "fileName"])
   }
   
+  ### from mouse gene names to human gene names based on gene orthology
+  convert_MG_to_HG <- function(mouse_gene_symbols, method=c("gene_symbol", "ensembl_id")) {
+    if(!require(biomaRt, quietly = TRUE)) {
+      if (!requireNamespace("BiocManager", quietly = TRUE))
+        install.packages("BiocManager")
+      BiocManager::install("biomaRt")
+      require(biomaRt, quietly = TRUE)
+    }
+    
+    ### get mouse-human map data
+    mouse_db = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+    mouse_human_gene_map <- getBM(attributes = c("external_gene_name", "ensembl_gene_id", "hsapiens_homolog_associated_gene_name", "hsapiens_homolog_ensembl_gene"), mart = mouse_db)
+    
+    ### only keep the genes that
+    mouse_human_gene_map <- mouse_human_gene_map[which(mouse_human_gene_map$external_gene_name %in% mouse_gene_symbols),]
+    
+    ### if there is duplicated mouse genes, use the first one
+    dups_idx <- which(duplicated(mouse_human_gene_map$external_gene_name))
+    if(length(dups_idx) > 0) {
+      mouse_human_gene_map <- mouse_human_gene_map[-dups_idx,]
+    }
+    
+    ### only keep the rows with human genes
+    if(method[1] == "gene_symbol") {
+      mouse_human_gene_map <- mouse_human_gene_map[which(mouse_human_gene_map$hsapiens_homolog_associated_gene_name != ""),]
+    } else {
+      mouse_human_gene_map <- mouse_human_gene_map[which(mouse_human_gene_map$hsapiens_homolog_ensembl_gene != ""),]
+    }
+    
+    ### set row names
+    rownames(mouse_human_gene_map) <- mouse_human_gene_map$external_gene_name
+    
+    ### convert the mouse gene symbols to human gene symbols
+    ### if none, return ""
+    if(method[1] == "gene_symbol") {
+      result <- mouse_human_gene_map[mouse_gene_symbols,"hsapiens_homolog_associated_gene_name"]
+    } else {
+      result <- mouse_human_gene_map[mouse_gene_symbols,"hsapiens_homolog_ensembl_gene"]
+    }
+    names(result) <- mouse_gene_symbols
+    
+    return(result)
+  }
+  
   ### get sub directories
   subdirs <- list.dirs(Robj_dir, recursive = FALSE)
   
@@ -54,9 +98,13 @@ cpdb_prep <- function(Robj_dir="Z:/ResearchHome/ProjectSpace/thomagrp/JCC282_Hem
       ### active assay = "RNA"
       Seurat_Obj@active.assay <- "RNA"
       
+      ### convert mouse gene names to human gene names
+      h_genes <- convert_MG_to_HG(rownames(Seurat_Obj@assays$RNA@counts), method = "ensembl_id")
+      h_genes <- h_genes[!is.na(h_genes)]
+      
       ### write out the input count data
-      write.table(data.frame(Gene=rownames(Seurat_Obj@assays$RNA@counts),
-                             Seurat_Obj@assays$RNA@counts,
+      write.table(data.frame(Gene=h_genes,
+                             data.frame(Seurat_Obj@assays$RNA@counts, stringsAsFactors = FALSE, check.names = FALSE)[names(h_genes),],
                              stringsAsFactors = FALSE, check.names = FALSE),
                   file = paste0(sd2, "/", strsplit(f, split = "_", fixed = TRUE)[[1]][1], "_counts.txt"),
                   sep = "\t", quote = FALSE, row.names = FALSE)
@@ -72,6 +120,10 @@ cpdb_prep <- function(Robj_dir="Z:/ResearchHome/ProjectSpace/thomagrp/JCC282_Hem
       if(length(unique(Seurat_Obj@meta.data$HSPC)) > 2) {
         writeLines(paste("ERROR:", f, "There are more than 2 classes in HSPC column."))
       }
+      
+      ### also write out the pre-built CellphoneDB command
+      writeLines(paste0("cellphonedb method statistical_analysis ",
+                        "/project_space/thomagrp/JCC282_Hematopoiesis/common/RNA-Magnet/20210412_Signaling_updated_p-values/Adult/LTHSC/AdultStromavLTHSC_meta.txt /project_space/thomagrp/JCC282_Hematopoiesis/common/RNA-Magnet/20210412_Signaling_updated_p-values/Adult/LTHSC/AdultStromavLTHSC_counts.txt --output-path=/project_space/thomagrp/JCC282_Hematopoiesis/common/RNA-Magnet/20210412_Signaling_updated_p-values/Adult/LTHSC/"))
     }
     
   }
