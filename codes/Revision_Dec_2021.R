732547a2db4ab0be694087251e00b983bacef706
hjkim88 <firadazer@gmail.com>
2022-01-27 22:28:46 -0600

    running RNAMagnet anchors.

--- codes/Revision_Dec_2021.R
+++ codes/Revision_Dec_2021.R
@@ -52,10 +52,16 @@ trent_revision <- function(inputDataPath="./data/Combined_Seurat_Obj.RDS",
     install.packages("reticulate")
     require(reticulate, quietly = TRUE)
   }
+  if(!require(RNAMagnet, quietly = TRUE)) {
+    remotes::install_github("veltenlab/rnamagnet")
+    require(RNAMagnet, quietly = TRUE)
+  }
+  if(!require(SeuratDisk, quietly = TRUE)) {
+    remotes::install_github("mojaveazure/seurat-disk")
+    require(SeuratDisk, quietly = TRUE)
+  }
   # remotes::install_github('chris-mcginnis-ucsf/DoubletFinder')
   # require(DoubletFinder, quietly = TRUE)
-  # remotes::install_github("mojaveazure/seurat-disk")
-  # require(SeuratDisk, quietly = TRUE)
   
   ## updated stroma RDS file
   Updated_Seurat_Obj <- readRDS(file = inputDataPath)
@@ -463,6 +469,221 @@ trent_revision <- function(inputDataPath="./data/Combined_Seurat_Obj.RDS",
   ### 2.) Pull out what they have annotated as HSC/MPP, recluster, and then perform GO analysis on the clusters
   ### to see if there is any overlap in the GO terms between their HSC/MPPs and our HSCs.
   
+  # Basic function to convert mouse to human gene names
+  convertMouseGeneList <- function(x){
+    if(!require(biomaRt, quietly = TRUE)) {
+      BiocManager::install("biomaRt")
+      require(biomaRt, quietly = TRUE)
+    }
+    
+    human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+    mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+    genesV2 = getLDS(attributes = c("mgi_symbol"), filters = "mgi_symbol", values = x , mart = mouse, attributesL = c("hgnc_symbol"), martL = human, uniqueRows=T)
+    
+    return(genesV2)
+  }
+  
+  # Basic function to convert human to mouse gene names
+  convertHumanGeneList <- function(x){
+    if(!require(biomaRt, quietly = TRUE)) {
+      BiocManager::install("biomaRt")
+      require(biomaRt, quietly = TRUE)
+    }
+    
+    human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+    mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+    genesV2 = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = x , mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
+    
+    return(genesV2)
+  }
+  
+  #'Low-level function to run core RNA magnet steps
+  #'
+  #' Users are advised to use the top-level functions \code{\link{RNAMagnetAnchors}} and \code{\link{RNAMagnetSignaling}} which appropriately set default parameters and return user-friendly return values.
+  #' This is a low level function for development purposes.
+  #'@param seurat An object of class \code{\link[Seurat]{seurat}} containing a valid clustering and t-SNE information
+  #'
+  #'. For information on how to create such an object, see https://satijalab.org/seurat/get_started.html
+  #'@param anchors A character vector of anchor populations. Entries must be levels of the seurat identity vector. If \code{NULL}: All entries of the seurat identity vector are used as anchors.
+  #'@param neighborhood.distance See \code{\link{RNAMagnetAnchors}}
+  #'@param neighborhood.gradient See \code{\link{RNAMagnetAnchors}}
+  #'@param .k Fuzzification parameter, see detail. Recommended to leave at the default value.
+  #'@param .x0 Fuzzification parameter, see detail. Recommended to leave at the default value.
+  #'@param .minExpression Minimal expression level of genes to be included, specified as the number of cells in the dataset that express the gene.
+  #'@param .minMolecules Number of molecules per cell required to count the cells as positive.
+  #'@param .version The version of the underlying ligand-receptor database. See \code{\link{getLigandsReceptors}}.
+  #'@param .cellularCompartment Types of ligands to be included. For physical interactions, defaults to \code{ c("Membrane","ECM","Both")}.  See \code{\link{getLigandsReceptors}}.
+  #'@param .manualAnnotation Annotation status of ligands to be included. Default to \code{"Correct"}. See \code{\link{getLigandsReceptors}}.
+  #'@param .symmetric Assume that if A is a receptor for B, B is also a receptor for A
+  #'@details The algorithm takes the following steps: \enumerate{
+  #'\item Ligand-receptor pairs are selected based on the parameters \code{.version}, \code{.cellularCompartment} and \code{.manualAnnotation}. Choice of \code{.cellularCompartment} is crucial for determining the algorithm's behavior, e.g. if set to \code{c("Secreted","Both")}, paracrine signaling interactions involving soluble ligands are investigated.
+  #'\item Dropout values in the expression levels of ligands and receptors are imputed using \code{\link[Rmagic]{magic}}
+  #'\item Mean expression level of ligands and receptors is computed for all anchor populations
+  #'\item For each cell or anchor population, the expression of each ligand and receptor is encoded as a fuzzy logic variable
+  #'\item Fuzzy logic AND is used to compute probabilities for a given interaction to be active between a single cell and an anchor population
+  #'\item An interaction score is computed as the sum of interaction probabilities across all possible ligand-receptor pairs
+  #'\item Specificty scores are computed by comparing interaction scores to average interaction scores in a local neighborhood.
+  #'}
+  #'@details Add the methods section of the paper here!
+  #'@return Returns an object of class \code{\link{rnamagnet}}
+  #'@export
+  RNAMagnetBase <- function(seurat, anchors=NULL, human=FALSE, neighborhood.distance=NULL, neighborhood.gradient =NULL, .k = 10, .x0 = 0.5, .minExpression,.minMolecules=1, .version = "1.0.0", .cellularCompartment, .manualAnnotation = "Correct", .symmetric = F) {
+    cat("Setting everything up...\n")
+    
+    if (grepl("^3", Biobase::package.version("Seurat")) || grepl("^4", Biobase::package.version("Seurat"))) {
+      seurat.ident <- Idents(seurat)
+      seurat.cell.names <- colnames(seurat)
+      seurat.pca <- Embeddings(seurat, reduction = "pca")
+      seurat.raw.data <- GetAssayData(seurat, slot = "counts")
+    } else {
+      seurat.ident <- seurat@ident
+      seurat.cell.names <- seurat@cell.names
+      seurat.pca <- seurat@dr$pca@cell.embeddings
+      seurat.raw.data <- seurat@raw.data
+    }
+    
+    ### if the input seurat is a human data
+    if(human) {
+      ### get mapping data
+      tempGenes <- convertHumanGeneList(rownames(seurat.raw.data))
+      
+      ### only retain genes those have mouse genes
+      seurat.raw.data <- seurat.raw.data[unique(tempGenes[,1]),]
+      
+      ### get human gene duplicates
+      dups <- unique(tempGenes[,1][which(duplicated(tempGenes[,1]))])
+      nonduplicated_tempGenes <- tempGenes[which(!(tempGenes[,1] %in% dups)),]
+      duplicated_tempGenes <- tempGenes[which(tempGenes[,1] %in% dups),]
+      
+      ### only retain genes those have mouse genes
+      seurat.raw.data2 <- seurat.raw.data[nonduplicated_tempGenes[,1],]
+      rownames(seurat.raw.data2) <- nonduplicated_tempGenes[,2]
+      seurat.raw.data3 <- seurat.raw.data[unique(duplicated_tempGenes[,1]),]
+      
+      ### one human gene can have multiple mouse genes
+      ### copy all the gexp for those multiple mouse genes
+      for(i in 1:nrow(seurat.raw.data3)) {
+        temp <- tempGenes[which(tempGenes[,1] == rownames(seurat.raw.data3)[i]),]
+        rownames(seurat.raw.data3)[i] <- temp[1,2]
+        for(j in 1:(nrow(temp)-1)) {
+          seurat.raw.data3 <- rbind(seurat.raw.data3, seurat.raw.data3[i,])
+        }
+        rownames(seurat.raw.data3)[(nrow(seurat.raw.data3)-nrow(temp)+2):nrow(seurat.raw.data3)] <- temp[2:nrow(temp),2]
+      }
+      
+      ### combine the partial results
+      seurat.raw.data <- rbind(seurat.raw.data2, seurat.raw.data3)
+      
+      ### check if adding multiple mouse genes were done accurately
+      print(identical(nrow(seurat.raw.data), nrow(tempGenes)))
+      print(identical(length(which(duplicated(rownames(seurat.raw.data)))), length(which(duplicated(tempGenes[,2])))))
+      
+      ### now there are duplicated mouse genes
+      ### get mean value for those
+      dup_idx <- which(duplicated(rownames(seurat.raw.data)))
+      dups <- unique(rownames(seurat.raw.data)[dup_idx])
+      for(dup in dups) {
+        target_idx <- which(rownames(seurat.raw.data) == dup)
+        seurat.raw.data[target_idx[1],] <- apply(seurat.raw.data[target_idx,], 2, median)
+      }
+      seurat.raw.data <- seurat.raw.data[-dup_idx,]
+    }
+    
+    if (is.null(anchors)) anchors <- as.character(unique(seurat.ident))
+    
+    out <- new("rnamagnet", celltype = seurat.ident, params = list("neighborhood.distance"=neighborhood.distance, "neighborhood.gradient" =neighborhood.gradient, ".k" = .k, ".x0" = .x0, ".minExpression" = .minExpression, ".minMolecules" = .minMolecules, ".cellularCompartment" = .cellularCompartment, ".manualAnnotation" = .manualAnnotation, ".symmetric" = .symmetric))
+    
+    #compute cell-cell similarity
+    similarity <- as.matrix(1-cor(t(seurat.pca[,1:15])))
+    
+    #prepare database
+    ligrec <- getLigandsReceptors(.version, .cellularCompartment, .manualAnnotation)
+    if (.symmetric) ligrec <- makeSymmetric(ligrec) #for physical interactions: i a binds b, b binds a.
+    
+    #prepare genes included into MAGIC
+    filteredGenes <- rownames(seurat.raw.data)[apply(seurat.raw.data[,seurat.cell.names] >=.minMolecules ,1,sum) > .minExpression]
+    
+    genes <- unique(c(ligrec$Receptor.Mouse,
+                      ligrec$Ligand.Mouse))
+    
+    genes <- genes[sapply(genes, function(x) {
+      entries <- strsplit(x, "[&|]")
+      if (grepl("&", x))
+        all(entries[[1]] %in% filteredGenes)
+      else any(entries[[1]] %in% filteredGenes)
+    })]
+    
+    genes_formagic <- unlist(strsplit( genes,"[&|]"))
+    
+    
+    formagic <- Matrix::t(seurat.raw.data[,seurat.cell.names])
+    formagic <- Rmagic::library.size.normalize(formagic)
+    formagic <- sqrt(formagic)
+    
+    #Run MAGIC
+    cat("Now running MAGIC to impute dropout values...\n")
+    mymagic <- Rmagic::magic(formagic, genes = genes_formagic, seed = 0xbeef)
+    mymagic <- as.data.frame(mymagic)
+    
+    #handle expression levels of heterodimers
+    resolvedRawData <- resolveData(t(mymagic), genes)
+    resolvedRawData <- t(apply(resolvedRawData, 1, function(x) (x - min(x )) / (max(x) - min(x)) ))
+    
+    
+    annotated_genes <- rownames(resolvedRawData)
+    out@mylr <- subset(ligrec, Receptor.Mouse %in% annotated_genes &
+                         Ligand.Mouse %in% annotated_genes)
+    
+    stepf<- Vectorize(function(x) if (x<0) 0 else x)
+    
+    cat("Now running RNAMagnet...\n")
+    
+    #compute mean gene expression level per population
+    out@anchors <- do.call(cbind, lapply(anchors, function(id) {
+      apply(resolvedRawData[,seurat.ident == id,drop=FALSE],1,mean)
+    }));
+    colnames(out@anchors) <- anchors
+    
+    #use fuzzy logic and operations to compute interaction score
+    out@interaction <- sapply(anchors, function(pop_l) {
+      out@mylr$expression_ligand <- out@anchors[out@mylr$Ligand.Mouse, pop_l]
+      sapply(seurat.cell.names, function(cell_r) {
+        out@mylr$expression_receptor <-resolvedRawData[out@mylr$Receptor.Mouse, cell_r]
+        sum(kernel(out@mylr$expression_ligand, k =.k, x0 = .x0) * kernel(out@mylr$expression_receptor, k = .k, x0=.x0 )) #kernel performs fuzzification
+      })
+    })
+    ### sometimes this is missing
+    colnames(out@interaction) <- anchors
+    
+    out@specificity <- t(sapply(rownames(out@interaction), function(cell) {
+      x <- out@interaction[cell,]
+      beta <- x/sum(x)
+      if (!is.null(neighborhood.distance)) alpha <- apply(out@interaction * (1-kernel(similarity[cell,],neighborhood.gradient,x0=neighborhood.distance )),2,sum) else alpha <- apply(out@interaction,2,mean)
+      alpha <- alpha / sum(alpha)
+      beta- alpha
+    }))
+    rownames(out@specificity) <- rownames(out@interaction)
+    
+    out@adhesiveness <- apply(mymagic, 1,function(x) sum(kernel(x,x0 = .x0, k = .k)))
+    return(out)
+    
+  }
+  
+  ### RNAMagnetAnchors
+  RNAMagnetAnchors <- function(seurat, anchors, human=FALSE, return = "summary", neighborhood.distance = 0.7, neighborhood.gradient = 3, .k = 10, .x0 = 0.5, .minExpression = 0, .minMolecules = 1, .version = "1.0.0", .cellularCompartment = c("Membrane","ECM","Both"), .manualAnnotation = "Correct" ) {
+    
+    myMagnet <- RNAMagnetBase(seurat, anchors, human, neighborhood.distance,neighborhood.gradient, .k, .x0, .minExpression, .minMolecules, .version, .cellularCompartment, .manualAnnotation,TRUE)
+    if (return=="rnamagnet-class") myMagnet else data.frame(direction = as.factor(colnames(myMagnet@specificity)[apply(myMagnet@specificity,1,which.max)]), adhesiveness = myMagnet@adhesiveness, myMagnet@specificity[,anchors])
+    
+  }
+  
+  ### RNAMagnetSignaling
+  RNAMagnetSignaling <- function(seurat, human=FALSE, neighborhood.distance = NULL, neighborhood.gradient = NULL, .k = 10, .x0 = 0.5, .minExpression = 10, .minMolecules = 1, .version = "1.0.0", .cellularCompartment = c("Secreted","Both"), .manualAnnotation = "Correct" ) {
+    
+    RNAMagnetBase(seurat, anchors = NULL, human, neighborhood.distance,neighborhood.gradient, .k, .x0, .minExpression, .minMolecules, .version, .cellularCompartment, .manualAnnotation, FALSE)
+    
+  }
+  
   # ### load the Jardine data
   # jardine_meta <- read.table(file = "./data/E-MTAB-9389.sdrf.txt",
   #                            sep = "\t", header = TRUE,
@@ -528,12 +749,37 @@ trent_revision <- function(inputDataPath="./data/Combined_Seurat_Obj.RDS",
   ### we got a combined and annotated data so load it instead of building from scratch
   
   ### convert h5ad to h5seurat and load
-  Convert(source = "./data/fig1b_fbm_scaled_gex_updated_dr_20210104.h5ad", dest = "h5seurat")
+  # Convert(source = "./data/fig1b_fbm_scaled_gex_updated_dr_20210104.h5ad", dest = "h5seurat")
   jardine_seurat2 <- LoadH5Seurat("./data/fig1b_fbm_scaled_gex_updated_dr_20210104.h5seurat")
   
-  ### https://satijalab.org/seurat/archive/v2.4/conversion_vignette.html
-  ad <- import("anndata", convert = FALSE)
-  obj <- ad$read_h5ad("./data/fig1b_fbm_scaled_gex_updated_dr_20210104.h5ad")
+  # ### https://satijalab.org/seurat/archive/v2.4/conversion_vignette.html
+  # ad <- import("anndata", convert = FALSE)
+  # obj <- ad$read_h5ad("./data/fig1b_fbm_scaled_gex_updated_dr_20210104.h5ad")
+  # jardine_seurat3 <- Convert(obj, to = "seurat")
+  
+  ### see UMAP of the h5ad file
+  DimPlot(object = jardine_seurat2, reduction = "umap", raster = TRUE,
+          group.by = "broad_fig1_cell.labels", pt.size = 1)
+  
+  ### pull out HSC/MPP and Stroma only
+  print(levels(jardine_seurat2$broad_fig1_cell.labels))
+  subset_jardine_obj <- subset(jardine_seurat2,
+                               cells = rownames(jardine_seurat2@meta.data)[which(jardine_seurat2$broad_fig1_cell.labels %in% c("HSC/MPP and pro", "stroma"))])
+  
+  ### PCA is not found so we run PCA
+  subset_jardine_obj <- FindVariableFeatures(subset_jardine_obj)
+  subset_jardine_obj <- ScaleData(subset_jardine_obj)
+  subset_jardine_obj <- RunPCA(subset_jardine_obj, npcs = 15)
+  
+  ### run RNAMagnet with anchors
+  subset_jardine_obj <- SetIdent(object = subset_jardine_obj,
+                                 cells = rownames(subset_jardine_obj@meta.data),
+                                 value = subset_jardine_obj$broad_fig1_cell.labels)
+  result <- RNAMagnetAnchors(seurat = subset_jardine_obj,
+                             anchors = unique(subset_jardine_obj$broad_fig1_cell.labels),
+                             human = TRUE)
+  
+  
   
   
   
